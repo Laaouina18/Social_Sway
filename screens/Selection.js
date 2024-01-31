529f0a6cb765fdfc69fc1c7aaaaf29b7083a4821
Laaouina18 <laaouinanouhaila2019@gmail.com>
2024-01-31 11:55:23 +0100

    SW-36   definir l'architecture du projet

---
+++ screens/Selection.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Selection = () => {
+  return (
+    <View>
+      <Text>Selection</Text>
+    </View>
+  )
+}
+
+export default Selection
---
+++ screens/index.js
@@ -0,0 +1,10 @@
+import Home from "./Home";
+import Selection from "./Selection";
+import Faqs from "./Faqs";
+import Favourite from "./Favourite";
+
+export {
+    Home,
+    Selection,
+    Faqs
+}
